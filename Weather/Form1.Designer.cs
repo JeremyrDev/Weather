042c83ddfa9e0d018a827305286b65f6f4fd0a0c
JeremyrDev <Jeremyr.dev@gmail.com>
2014-11-05 02:00:36 -0500

    Two services added, auto location, 2 minute continuous refresh

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -28,59 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "0";
             // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(284, 262);
+            this.panel1.Size = new System.Drawing.Size(257, 193);
             this.panel1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(197, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 97);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(12, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Weather\r\nUnderground\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(133, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Forecast.IO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(133, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 120000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(257, 193);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -95,8 +130,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Weather/Form1.cs
+++ Weather/Form1.cs
@@ -14,113 +14,131 @@ namespace Weather
 {
     public partial class Form1 : Form
     {
-        WebClient w;
         public Form1()
         {
             InitializeComponent();
         }
-        string json = @"{
-              'Name': 'Bad Boys',
-              'ReleaseDate': '1995-4-7T00:00:00',
-              'Genres': [
-                'Action',
-                'Comedy'
-              ]
-               }";
+        string json;
+        string json2;
+        string json3;
+        string zipcode;
+        string state;
+        string city;
+        string location;
+        int updateCounter = 0;
 
-        public class Item
-        {
-            public int millis;
-            public string stamp;
-            public DateTime datetime;
-            public string light;
-            public float temp;
-            public float vcc;
-        }
-        public void LoadJson()
-        {
-            label1.Text = "Started";
-            using (var webClient = new System.Net.WebClient())
-            {
-                var json = webClient.DownloadString("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/40.935277,-80.361621");
-                // Now parse with JSON.Net
-            }
-            using (StreamReader r = new StreamReader(json))
-            {
-                string jsons = r.ReadToEnd();
-                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(jsons);
-
-                dynamic array = JsonConvert.DeserializeObject(jsons);
-                foreach (var item in array)
-                {
-                    label1.Text = String.Format("{0}", item);
-                }
-            }
-        }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            getData();
         }
 
         void getData()
         {
-            //https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/40.935277,-80.361621
-
-             //var temp = JsonConvert.DeserializeObject(json);
-
-            //label1.Text = temp.Name;
-
-//            string json = @"{
-//            'CPU': 'Intel',
-//            'PSU': '500W',
-//            'Drives': [
-//              'DVD read/writer'
-//             /*(broken)*/,
-//             '500 gigabyte hard drive',
-//             '200 gigabype hard drive'
-//            ]
-//        }";
-            label1.Text = "Started"+Environment.NewLine;
-            //w = new WebClient();
-            //w.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/40.935277,-80.361621", @"E:\Users\Jeremy-SSDOS\Desktop\j.txt");
-
-            using (WebClient Client = new WebClient())
+            json = "";
+            json2 = "";
+            json3 = "";
+            using (WebClient Client2 = new WebClient()) // get location
             {
-                Client.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/40.935277,-80.361621", "j.txt");
+                Client2.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/geolookup/q/autoip.json", "j2.txt");
+
+                int counter2 = 0;
+                string line2;
+
+                // Read the file and display it line by line.
+                StreamReader file2 = new StreamReader("j2.txt");
+                while ((line2 = file2.ReadLine()) != null)
+                {
+                    json2 += line2;
+                    counter2++;
+                }
+
+                file2.Close();
+
+                var results = JsonConvert.DeserializeObject<dynamic>(json2);
+                var id = results.location;
+                var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
+                location = mresults.lat+","+mresults.lon;
+                zipcode = mresults.zip;
+                state = mresults.state;
+                city = mresults.city;
+
+                label1.Text = city +", "+state+ " "+zipcode +Environment.NewLine+location;
             }
-                // Now parse with JSON.Net
-
-            //label1.Text += File.ReadAllText(@"E:\Users\Jeremy-SSDOS\Desktop\j.txt");
-            int counter = 0;
-            string line;
-
-            // Read the file and display it line by line.
-            StreamReader file = new StreamReader("j.txt");
-            while ((line = file.ReadLine()) != null)
+            using (WebClient Client = new WebClient()) //get weather data from location -- weather underground
             {
-               label2.Text += counter.ToString()+" : "+line+Environment.NewLine;
-                counter++;
+                //Client.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/"+location, "j.txt");
+                Client.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/conditions/q/"+state+"/"+city+".json", "j.txt");
+                int counter = 0;
+                string line;
+
+                // Read the file and display it line by line.
+                StreamReader file = new StreamReader("j.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    json += line;
+                    counter++;
+                }
+
+                file.Close();
+
+                var results = JsonConvert.DeserializeObject<dynamic>(json);
+                var id = results.current_observation;
+                var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
+                var summary = mresults.weather;
+                var temperature = mresults.temperature_string;
+                var humidity = mresults.relative_humidity;
+
+                label2.Text = summary + Environment.NewLine +
+                    temperature + Environment.NewLine +
+                    humidity;
             }
+            using (WebClient Client3 = new WebClient()) //get weather data from location -- forecast.IO
+            {
+                Client3.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/"+location, "j3.txt");
+                //Client.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/conditions/q/" + state + "/" + city + ".json", "j.txt");
+                int counter = 0;
+                string line;
 
-            file.Close();
+                // Read the file and display it line by line.
+                StreamReader file = new StreamReader("j3.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    json3 += line;
+                    counter++;
+                }
 
-            label1.Text += counter+" : Ended" + Environment.NewLine;
-            //JsonTextReader reader = new JsonTextReader(new StringReader(@"E:\Users\Jeremy-SSDOS\Desktop\j.json"));
-            //while (reader.Read())
-            //{
-            //    if (reader.Value != null)
-            //       label1.Text += String.Format("Token: {0}, Value: {1}"+Environment.NewLine, reader.TokenType, reader.Value);
-            //   else
-            //        label1.Text += String.Format("Token: {0}", reader.TokenType);
-            //}
+                file.Close();
 
-            
+                var results = JsonConvert.DeserializeObject<dynamic>(json3);
+                var id = results.currently;
+                var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
+                var summary = mresults.summary;
+                var temperature = mresults.temperature;
+                var humidity = mresults.humidity;
+
+                label5.Text = summary + Environment.NewLine +
+                    temperature + Environment.NewLine +
+                    humidity;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            getData();
+            updateCounter++;
+            this.Text = updateCounter.ToString();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             getData();
+            updateCounter++;
+            this.Text = updateCounter.ToString();
         }
     }
 }
