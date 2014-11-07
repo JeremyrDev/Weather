3d3dbc5dafe3ab14a63b039b4e1210c9881ae423
JeremyrDev <Jeremyr.dev@gmail.com>
2014-11-07 00:54:46 -0500

    More data, optimization

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -38,6 +39,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,15 +47,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(86, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "0";
+            this.label1.Text = "--";
             // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label5);
@@ -64,24 +68,35 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(328, 193);
+            this.panel1.Size = new System.Drawing.Size(394, 258);
             this.panel1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(3, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 247);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "City, State, Zip\r\nLat, Lon\r\nWOEID\r\n\r\n\r\nService\r\n\r\n\r\nConditions\r\nTemperature\r\nHumi" +
+    "dity\r\nWind\r\nFeels Like\r\nPressure\r\n\r\nSunrise/set\r\n\r\nForecast\r\nTime";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label6.Location = new System.Drawing.Point(229, 97);
+            this.label6.Location = new System.Drawing.Point(292, 112);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(13, 13);
             this.label6.TabIndex = 6;
-            this.label6.Text = "0";
+            this.label6.Text = "--";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(229, 67);
+            this.label7.Location = new System.Drawing.Point(292, 74);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 13);
             this.label7.TabIndex = 5;
@@ -91,17 +106,17 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label5.Location = new System.Drawing.Point(122, 97);
+            this.label5.Location = new System.Drawing.Point(185, 112);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(13, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "0";
+            this.label5.Text = "--";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(122, 67);
+            this.label4.Location = new System.Drawing.Point(185, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(62, 13);
             this.label4.TabIndex = 3;
@@ -111,7 +126,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(12, 54);
+            this.label3.Location = new System.Drawing.Point(85, 61);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 26);
             this.label3.TabIndex = 2;
@@ -121,24 +136,34 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Location = new System.Drawing.Point(85, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "0";
+            this.label2.Text = "--";
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 120000;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(351, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.ClientSize = new System.Drawing.Size(328, 193);
+            this.ClientSize = new System.Drawing.Size(394, 258);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -160,6 +185,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- Weather/Form1.cs
+++ Weather/Form1.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Net;
 using System.Xml;
 using System.Globalization;
+
 namespace Weather
 {
     public partial class Form1 : Form
@@ -23,7 +24,6 @@ namespace Weather
         string json;
         string json2;
         string json3;
-        string xml;
         string zipcode;
         string state;
         string city;
@@ -32,32 +32,18 @@ namespace Weather
         string location;
         string WOEID;
         int updateCounter = 0;
-
+        int timer = 0;
+        bool started = false;
         private void Form1_Load(object sender, EventArgs e)
         {
-            getLocation();
-            getData();
+            timer = 119;
         }
         void getLocation()
         {
             json2 = "";
-            xml = "";
-            using (WebClient Client = new WebClient()) // get location weatherunderground
+            using (WebClient c = new WebClient()) // get location weatherunderground
             {
-                Client.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/geolookup/q/autoip.json", "j2.txt");
-
-                int counter = 0;
-                string line;
-
-                // Read the file and display it line by line.
-                StreamReader file = new StreamReader("j2.txt");
-                while ((line = file.ReadLine()) != null)
-                {
-                    json2 += line;
-                    counter++;
-                }
-
-                file.Close();
+                json2 = c.DownloadString("http://api.wunderground.com/api/b0ad6743f596f56f/geolookup/q/autoip.json");
 
                 var results = JsonConvert.DeserializeObject<dynamic>(json2);
                 var id = results.location;
@@ -71,121 +57,144 @@ namespace Weather
 
                 label1.Text = city + ", " + state + " " + zipcode + Environment.NewLine + location+Environment.NewLine;
             }
-            using (WebClient Client = new WebClient()) // get location yahoo with WOEID
-            {
-                Client.DownloadFile("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22"+lat+"%2C"+lon+"%22%20and%20gflags%3D%22R%22", "x.xml");
+            //get location from yahoo with WOEID
+            string query = String.Format("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22" + lat + "%2C" + lon + "%22%20and%20gflags%3D%22R%22");
 
-                int counter = 0;
-                string line;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(query);
 
-                // Read the file and display it line by line.
-                StreamReader file = new StreamReader("x.xml");
-                while ((line = file.ReadLine()) != null)
-                {
-                    xml += line;
-                    counter++;
-                }
+            XmlNode node = doc.SelectSingleNode("query");
+            XmlNode node2 = node.SelectSingleNode("results");
+            XmlNode node3 = node2.SelectSingleNode("Result");
+            WOEID = node3.SelectSingleNode("woeid").InnerText;
+            label1.Text += WOEID;
 
-                file.Close();
-
-                XmlDocument doc = new XmlDocument();
-                doc.Load("x.xml");
-
-                XmlNode node = doc.SelectSingleNode("query");
-                XmlNode node2 = node.SelectSingleNode("results");
-                XmlNode node3 = node2.SelectSingleNode("Result");
-                WOEID = node3.SelectSingleNode("woeid").InnerText;
-                label1.Text += WOEID;
-            }
         }
         void getData()
         {
             json = "";
             json3 = "";
-            using (WebClient Client = new WebClient()) //get weather data from location -- weather underground
+            //get weatherunderground data
+            try
             {
-                //Client.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/"+location, "j.txt");
-                Client.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/conditions/q/"+state+"/"+city+".json", "j.txt");
-                int counter = 0;
-                string line;
-
-                // Read the file and display it line by line.
-                StreamReader file = new StreamReader("j.txt");
-                while ((line = file.ReadLine()) != null)
+                using (var webClient = new System.Net.WebClient())
                 {
-                    json += line;
-                    counter++;
+                    json = webClient.DownloadString("http://api.wunderground.com/api/b0ad6743f596f56f/conditions/q/" + state + "/" + city + ".json");
+
+                    var results = JsonConvert.DeserializeObject<dynamic>(json);
+                    var id = results.current_observation;
+                    var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
+                    var summary = mresults.weather;
+                    var temperature = mresults.temperature_string;
+                    var humidity = mresults.relative_humidity;
+                    var wind = mresults.wind_mph;
+                    var feelsLike = mresults.feelslike_string;
+                    var pressure = mresults.pressure_in;
+                    label2.Text = summary + Environment.NewLine +
+                        temperature + Environment.NewLine +
+                        humidity + Environment.NewLine+
+                        wind + Environment.NewLine+
+                        feelsLike + Environment.NewLine+
+                        pressure;
                 }
-
-                file.Close();
-
-                var results = JsonConvert.DeserializeObject<dynamic>(json);
-                var id = results.current_observation;
-                var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
-                var summary = mresults.weather;
-                var temperature = mresults.temperature_string;
-                var humidity = mresults.relative_humidity;
-
-                label2.Text = summary + Environment.NewLine +
-                    temperature + Environment.NewLine +
-                    humidity;
             }
-            using (WebClient Client = new WebClient()) //get weather data from location -- forecast.IO
+            catch
             {
-                Client.DownloadFile("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/"+location, "j3.txt");
-                //Client.DownloadFile("http://api.wunderground.com/api/b0ad6743f596f56f/conditions/q/" + state + "/" + city + ".json", "j.txt");
-                int counter = 0;
-                string line;
+                label2.Text = "failed";
+            }
 
-                // Read the file and display it line by line.
-                StreamReader file = new StreamReader("j3.txt");
-                while ((line = file.ReadLine()) != null)
+            //get forecast.IO weather data
+            try
+            {
+                using (var webClient = new System.Net.WebClient())
                 {
-                    json3 += line;
-                    counter++;
-                }
+                    json3 = webClient.DownloadString("https://api.forecast.io/forecast/e27b59d61b3f8e9c51f71fdc33b15018/"+location);
 
-                file.Close();
-                try
-                {
                     var results = JsonConvert.DeserializeObject<dynamic>(json3);
                     var id = results.currently;
                     var mresults = JsonConvert.DeserializeObject<dynamic>(id.ToString());
                     var summary = mresults.summary;
-                    var temperature = mresults.temperature;
+                    string temperature = mresults.temperature;
                     string humidity = mresults.humidity;
-                    string h2 = (float.Parse(humidity, CultureInfo.CurrentCulture)*100).ToString("f0") + "%";
+                    string wind = mresults.windSpeed;
+                    string pressure = mresults.pressure;
+                    var h2 = (float.Parse(humidity, CultureInfo.CurrentCulture) * 100).ToString("f0") + "%";
+                    
+                    //Calculate windchil
+                    float w = float.Parse(wind, CultureInfo.InvariantCulture);
+
+                    float t = float.Parse(temperature, CultureInfo.CurrentCulture);
+
+                    double wc = (35.74 + (0.6215*t)) - (35.75 * (Math.Pow(w, .016))) + ((0.4275 * t) * (Math.Pow(w, .016)));
+
+                    string feelsLike = wc.ToString("f2")+"°F";
+
+                    float temp = float.Parse(pressure, CultureInfo.CurrentCulture);
+                    double p = temp * 0.0295301;
+                    
                     label5.Text = summary + Environment.NewLine +
-                        temperature+"°F" + Environment.NewLine +
-                        h2;
-                }
-                catch
-                {
-                    label5.Text = "failed";
+                        temperature + "°F" + Environment.NewLine +
+                        h2 + Environment.NewLine +
+                        wind +"mph" + Environment.NewLine+
+                        feelsLike + Environment.NewLine +
+                        p.ToString("f2");
                 }
             }
-            String query = String.Format("http://weather.yahooapis.com/forecastrss?w=" + WOEID);
-            XmlDocument d = new XmlDocument();
-            d.Load(query);
+            catch
+            {
+                label5.Text = "failed";
+            }
+            //get yahoo weather data
+            try
+            {
+                
+                string query = String.Format("http://weather.yahooapis.com/forecastrss?w=" + WOEID);
+                XmlDocument d = new XmlDocument();
+                d.Load(query);
 
-            XmlNamespaceManager m = new XmlNamespaceManager(d.NameTable);
-            m.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+                XmlNamespaceManager m = new XmlNamespaceManager(d.NameTable);
+                m.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
 
-            XmlNode channel = d.SelectSingleNode("rss").SelectSingleNode("channel");
-            XmlNodeList nodes = d.SelectNodes("rss/channel/item/yweather:forecast", m);
+                XmlNode channel = d.SelectSingleNode("rss").SelectSingleNode("channel");
+                XmlNodeList nodes = d.SelectNodes("rss/channel/item/yweather:forecast", m);
 
-            string condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", m).Attributes["text"].Value;
-            string t = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", m).Attributes["temp"].Value + "°F";
-            string h = channel.SelectSingleNode("yweather:atmosphere", m).Attributes["humidity"].Value + "%";
+                string condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", m).Attributes["text"].Value;
+                string t = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", m).Attributes["temp"].Value + "°F";
+                string h = channel.SelectSingleNode("yweather:atmosphere", m).Attributes["humidity"].Value + "%";
+                string p = channel.SelectSingleNode("yweather:atmosphere", m).Attributes["pressure"].Value;
+                string w = channel.SelectSingleNode("yweather:wind", m).Attributes["speed"].Value + "mph";
+                string wc = channel.SelectSingleNode("yweather:wind", m).Attributes["chill"].Value + "°F";
+                string sr = channel.SelectSingleNode("yweather:astronomy", m).Attributes["sunrise"].Value;
+                string ss = channel.SelectSingleNode("yweather:astronomy", m).Attributes["sunset"].Value;
 
-            label6.Text = condition + Environment.NewLine + t + Environment.NewLine + h;
+                label6.Text = condition + Environment.NewLine + t + Environment.NewLine + h +Environment.NewLine+
+                    w+Environment.NewLine+
+                    wc+Environment.NewLine+
+                        p+Environment.NewLine+Environment.NewLine+
+                        sr+Environment.NewLine+
+                        ss;
+            }
+            catch
+            {
+                label5.Text = "failed";
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            getData();
-            updateCounter++;
-            this.Text = updateCounter.ToString();
+            timer++;
+            if (timer > 119)
+            {
+                if(!started)
+                {
+                    started = true;
+                    getLocation();
+                }
+                getData();
+                updateCounter++;
+                label9.Text = updateCounter.ToString();
+                timer = 0;
+            }
         }
     }
 }
